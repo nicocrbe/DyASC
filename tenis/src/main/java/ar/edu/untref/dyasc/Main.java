c496c3b2e6deabc3a8b628fe7c8ea8a0ce152f9c
nicocrbe <nicolas.crbe@gmail.com>
2023-10-01 22:34:46 -0300

    Se agregan tests de casos no exitosos de ganar set por tiebreak y se refactoriza el codigo de partido service para que sume puntos en tiebreak con el sumar game local/visitante

--- tenis/src/main/java/ar/edu/untref/dyasc/Main.java
+++ tenis/src/main/java/ar/edu/untref/dyasc/Main.java
@@ -2,7 +2,7 @@ package ar.edu.untref.dyasc;
 
 public class Main {
 
-    public sttc void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
