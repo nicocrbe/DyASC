4b532a33e33d3698bea5f208ed63c56f70581911
Nicolás Bellesi <nicolas.crbe@gmail.com>
2023-10-12 00:17:47 -0300

    test fallido2

--- tenis/src/main/java/ar/edu/untref/dyasc/Main.java
+++ tenis/src/main/java/ar/edu/untref/dyasc/Main.java
@@ -3,6 +3,6 @@ package ar.edu.untref.dyasc;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.ogut.println("Hello World!");
     }
 }
