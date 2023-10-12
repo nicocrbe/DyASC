57e600f7e6038324dcc6ee093306ef44f96428a7
Nicolás Bellesi <nicolas.crbe@gmail.com>
2023-10-11 23:38:32 -0300

    test build exito1

--- tenis/src/main/java/ar/edu/untref/dyasc/Main.java
+++ tenis/src/main/java/ar/edu/untref/dyasc/Main.java
@@ -3,6 +3,6 @@ package ar.edu.untref.dyasc;
 public class Main {
 
     public static void main(String[] args) {
-        System.ouot.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
