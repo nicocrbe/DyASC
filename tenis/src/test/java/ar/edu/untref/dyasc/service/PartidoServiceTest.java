08c484b6a69c01a6885260874358e0521a0f021f
nicocrbe <nicolas.crbe@gmail.com>
2023-11-09 18:09:59 -0300

    d

--- tenis/src/test/java/ar/edu/untref/dyasc/service/PartidoServiceTest.java
+++ tenis/src/test/java/ar/edu/untref/dyasc/service/PartidoServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class PartidoServiceTest {
+public classPartidoServiceTest {
 
     private Jugador jugadorLocal;
     private Jugador jugadorVisitante;
